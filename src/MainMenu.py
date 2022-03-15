77dc37c879391ae8fa478f692942894272f48b6a
jpsorondo <sorondojp@gmail.com>
2022-03-15 00:09:24 -0300

    Minor fixes

--- src/MainMenu.py
+++ src/MainMenu.py
@@ -3,12 +3,15 @@ from src.obj.AllEntriesObject import AllEntriesObject
 
 class MainMenu:
 
-    def __init__(self, filepath):
+    def __init__(self, filepath, email, api_token, organization):
+        self.email = email
+        self.api_token = api_token
+        self.organization = organization
         self.all_entries = AllEntriesObject(filepath)
 
-    def jira_logger(self, email, api_token, organization):
+    def jira_logger(self):
         entry_response_map = {}
-        responses = JiraTimeLogger.jira_log_time(email, api_token, organization, self.all_entries.get_jira_entries())
+        responses = JiraTimeLogger.jira_log_time(self.email, self.api_token, self.organization, self.all_entries.get_jira_entries())
         for i in range(len(responses)):
             entry_response_map[str(self.all_entries.jira_entries[i].to_string())] = str(responses[i].status_code)
         return entry_response_map
