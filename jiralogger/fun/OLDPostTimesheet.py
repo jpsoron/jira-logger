81f51d15ba824d6fe310943be906b15613c02d8c
jpsorondo <sorondojp@gmail.com>
2022-12-17 22:35:33 -0300

    Implemented Requests library's Sessions object - Performance improvements around 50%

--- jiralogger/fun/OLDPostTimesheet.py
+++ jiralogger/fun/OLDPostTimesheet.py
@@ -6,15 +6,13 @@ import json
 
 
 def post_timesheet(email, api_token, organization, timesheet):
-    session = requests.session()
+    # Doesn't use requests.sessions
     auth = HTTPBasicAuth(email, api_token)
     headers = {
         "Accept": "application/json",
         "Content-Type": "application/json"
     }
-    session.auth = auth
-    session.headers.update(headers)
-    account_id = get_self_userid(session, organization)
+    account_id = get_self_userid(auth, organization, headers)
     worklog_entries = timesheet.get_worklog_entries()
     for worklog_entry in worklog_entries:
         project = worklog_entry.project
@@ -25,32 +23,36 @@ def post_timesheet(email, api_token, organization, timesheet):
         started_after_param = unix_time_convert(worklog_entry.date - datetime.timedelta(seconds=1))
         started_before_param = unix_time_convert(worklog_entry.date + datetime.timedelta(seconds=1))
 
-        if validate_non_duplicate(session, organization, account_id, project, issue_num, time_spent_seconds, comment, started_after_param, started_before_param):
-            response = post_entry(session, organization, project, issue_num, time_spent_seconds, comment, started_date)
+        if validate_non_duplicate(auth, organization, headers, account_id, project, issue_num, time_spent_seconds, comment, started_after_param, started_before_param):
+            response = post_entry(auth, organization, headers, project, issue_num, time_spent_seconds, comment, started_date)
             print("Entry: {" + started_date + " " + project + "-" + str(issue_num) + ": " + str(comment) + "}" + " Status: " + "{" + str(response.status_code) + "}")
         else:
             print("Entry: {" + started_date + " " + project + "-" + str(issue_num) + ": " + str(comment) + "}" + " Status: " + "{DUPLICATE ENTRY}")
 
 
-def post_entry(session, organization, project, issue_num, time_spent_seconds, comment, started_date):
+def post_entry(auth, organization, headers, project, issue_num, time_spent_seconds, comment, started_date):
     payload = json.dumps({
         "timeSpentSeconds": time_spent_seconds,
         "comment": comment,
         "started": started_date
     })
-    response = session.request(
+    response = requests.request(
         "POST",
         url="https://" + organization + ".atlassian.net/rest/api/2/issue/" + project + "-" + str(issue_num) + "/worklog",
-        data=payload
+        data=payload,
+        headers=headers,
+        auth=auth
     )
     return response
 
 
-def validate_non_duplicate(session, organization, account_id, project, issue_num, time_spent_seconds, comment, started_after, started_before):
+def validate_non_duplicate(auth, organization, headers, account_id, project, issue_num, time_spent_seconds, comment, started_after, started_before):
 
-    response = session.request(
+    response = requests.request(
         "GET",
-        url="https://" + organization + ".atlassian.net/rest/api/2/issue/" + project + "-" + str(issue_num) + "/worklog?startedAfter=" + str(started_after) + "&" + str(started_before)
+        url="https://" + organization + ".atlassian.net/rest/api/2/issue/" + project + "-" + str(issue_num) + "/worklog?startedAfter=" + str(started_after) + "&" + str(started_before),
+        headers=headers,
+        auth=auth
     )
 
     issue_worklogs = json.loads(response.text)["worklogs"]
@@ -64,9 +66,11 @@ def unix_time_convert(timestamp):
     return int(time.mktime(timestamp.timetuple()) * 1e3 + timestamp.microsecond / 1e3)
 
 
-def get_self_userid(session, organization):
-    response = session.request(
+def get_self_userid(auth, organization, headers):
+    response = requests.request(
         "GET",
-        url="https://" + organization + ".atlassian.net/rest/api/2/myself"
+        url="https://" + organization + ".atlassian.net/rest/api/2/myself",
+        headers=headers,
+        auth=auth
     )
     return json.loads(response.text)["accountId"]
